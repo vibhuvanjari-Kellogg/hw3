6644389d1c05e566bb36589781b3076eb9a727b9
Vibhu Vanjari <vibhu.vanjari@kellogg.northwestern.edu>
2025-02-25 02:53:45 +0000

    got "/places" working

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+  
+  get("/places", :controller => "places", :action => "index")
+
+
 end
